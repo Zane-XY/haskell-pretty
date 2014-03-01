9bfc90fc4626ee015565e3942aecb2cdb473baec
Zane.XY.W <_@_.com>
2014-03-01 14:39:45 +0800

    use prettyPrint function

--- test/PrettySpec.hs
+++ test/PrettySpec.hs
@@ -6,9 +6,13 @@ import Pretty
 main :: IO ()
 main = hspec spec
 
+
 spec :: Spec
-spec = describe "someFunction" $
-    it "should work fine" $ do
-      putStrLn (prettyHS " module X       where x = 1 where { y 1 = 2; y _ = 2  } --x")
-      content <- prettyHSFile "test/PrettySpec.hs"
-      putStrLn content
+spec
+  = describe "someFunction" $
+      it "should work fine" $
+        do putStrLn
+             (prettyHS
+                " module X       where x = 1 where { y 1 = 2; y _ = 2  } --x")
+           content <- prettyHSFile "test/PrettySpec.hs"
+           putStrLn content
